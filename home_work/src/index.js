737ff4443df8e8d1b5b7f5301dfe11a1d2725a27
ivan <ivanpavlenko871@gmail.com>
2020-07-02 21:56:01 +0300

    commit

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -8,17 +8,24 @@ function plural(heightTriangle , symbol) {
     symbol = prompt('Введите символ');
     
     let space = ' ';
+    let maxSymbols = heightTriangle * 2 - 1;
 
     if( typeof heightTriangle === 'number' && symbol.length === 1) {
+        
+        for (let i = 1; i <= maxSymbols; i += 2) {
 
-        for (let i = 1; i <= heightTriangle; i++) {
-    
-            console.log(space.repeat(heightTriangle - i ) + symbol.repeat(i)) 
+                let spaces = (maxSymbols-i)/2;
+
+                console.log(space.repeat(spaces) + symbol.repeat(i))
+             
         }
     }     
 }
 
 plural()
+
+
+
 
 
 
