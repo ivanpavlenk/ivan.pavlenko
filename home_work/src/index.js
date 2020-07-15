bfcfbcafb965cfa8566c40c9f8c57a35d59eb328
ivan <ivanpavlenko871@gmail.com>
2020-07-15 03:55:42 +0300

    homework8

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,51 +1,107 @@
-function odd (myArray) {
-    let newArray = []
-    for (let i = 0; i < myArray.length; i++) {
+// let arr = [1,2,3,4,5]
+
+// function myMap(array , callBack) {
+
+//     let newArr =[];
+
+//     for (let i = 0; i < array.length; i++) {
         
-        if (myArray[i] % 2 != 0) {
-            
-            newArray.push(myArray[i])
-        }
+//         newArr.push(callBack(array[i]));
         
-    } return newArray
-
-}
-
-alert(odd([1,2,3,4,5,6,7,8]))
-
-
-
-
-// function even (myArray) {
-
-//     let newArray = []
-
-//     for (let i = 0; i < myArray.length; i++) {
-        
-//         if (myArray[i] % 2 == 0) {
-            
-//             newArray.push(myArray[i])
-//         }
-        
-//     } return newArray
+//     }   
+//     return newArr
 // }
-// alert(even([1,2,3,4,5,6,7,8]))
+
+// alert(myMap(arr , function (elem) {
+
+//     return elem * 2
+// }))
 
 
 
 
-// let needArray = [[1,2,3,4],[5,6,7,8]]
 
-// function flat (myArray) {
-    
-//     newArray = []
 
-//     for (let i = 0; i < myArray.length; i++) {
-        
-//         for (let k = 0; k < myArray[i].length; k++) {
-        
-//            newArray.push(myArray[i][k]) 
+// let arr = [1,2,3,4,5]
+
+// function myFilter(array , callBack) {
+
+//     let newArr =[];
+
+//     for (let i = 0; i < array.length; i++) {
+//         let condition = callBack(array[i])
+
+//         if (condition === true) {
+            
+//             newArr.push(array[i]);
 //         } 
-//     } return newArray
+//     }   
+//     return newArr
 // }
-// alert(flat(needArray))
+
+// alert(myFilter(arr , function (elem) {
+
+//     return elem * 2 < 10
+// }))
+
+
+
+
+
+const notification = [
+
+    {
+   
+     date: '31/07/2019',
+   
+     msg: 'Some message here'
+   
+    },
+
+    {
+   
+        date: '31/07/2019',
+      
+        msg: 'Some message here2'
+      
+    },
+
+
+    {
+   
+        date: '31/07/2017',
+      
+        msg: 'Some message here3'
+      
+    },
+   
+   ];
+
+        function getGroup(arr , groupBy) {
+            
+            return arr.reduce(function(acc, item) {
+
+                let key = item[groupBy]
+        
+                if (!acc[key]) {
+        
+                    acc[key] = []
+                }
+        
+                acc[key].push(item)
+    
+                    return acc
+        
+               },{})
+
+        }
+
+        let newArr = getGroup(notification,'date')
+
+        console.log(newArr);
+
+       
+       
+    
+    
+
