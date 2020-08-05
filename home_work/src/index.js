134ca5acf70c2450bd8438cc2be1ce4b5263ddfe
ivan <ivanpavlenko871@gmail.com>
2020-08-06 00:52:38 +0300

    homework13

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,135 +1,210 @@
 
-let toDo = {
-   
-    items: [],
+function TodoCreate () {
 
-    createNewTask: function (taskName,taskStatus,taskText,taskDate){
-
-        taskDate = new Date().toLocaleString();
-
-        if (toDo.items.length > 0) {
-
-            for (const [key,value] of Object.entries(toDo.items)) {
-
-                if (value.name !== taskName) {
-
-                   let task = Object.create(
-                    {},
-        
-                    {
-                        name:{
-                            value: taskName
-                        },
-        
-                        status: {
-                            value: taskStatus,
-                            writable: true,
-                        },
-        
-                        date: {
-                            value: taskDate
-                        },
-        
-                        text: {
-                            value: taskText,
-                            writable:  true
-                        }
-        
-                    });
-                this.items.push(task);
-                    
-                }
-            }
-            
-        }
-        else {
-            let task = Object.create(
-                {},
-    
-                {
-                    name:{
-                        value: taskName
-                    },
-    
-                    status: {
-                        value: taskStatus,
-                        writable: true,
-                    },
-    
-                    date: {
-                        value: taskDate
-                    },
-    
-                    text: {
-                        value: taskText,
-                        writable:  true
-                    }
-    
-                });
-            this.items.push(task);
-        }
-    },
-
-    deleteTask: function (name, apply) {
-
-            if (apply === true) {
-
-                this.items = this.items.filter(task => task.name !== name ) 
-            }  
-        },
-        
-    editTask: function (taskName, newText, confirm) {
-
-        if (confirm === true) {
-            
-            this.items = this.items.map(function(task) {
-
-                if (task.name === taskName) {
-
-                    task.text = newText
-                }
-                return task
-            })
-        }
-       
-    },
-
-    showInfoAboutTasks: function () {
-
-        let allTask = 0;
-
-        let activeTask = 0;
-        
-        let completedTask = 0;
-
-        for (const [key,value] of Object.entries(toDo.items)) {
-
-             allTask++;
-
-             if (value.status === true) {
-
-                 activeTask++;
-                 
-             } else completedTask++;
-
-        }
-        console.log(`Всего задач - ${allTask}. Активных - ${activeTask}. Сделано - ${completedTask}`)
-    }
-
+    this.tasks = []
 }
 
-toDo.createNewTask('task1', true, 'do bla bla bla');
-toDo.createNewTask('task2', false, 'do bla bla bla');
-toDo.editTask('task1', 'dfhdfh',true)
-console.log(toDo.items);
-toDo.deleteTask('task2', true);
-toDo.editTask('task1','fghfghfghfghfghfghfghfghfgh', true)
-console.log(toDo.items);
+TodoCreate.prototype.createTask = function(taskName,taskDate) {
 
+    taskDate = new Date().toLocaleString();
 
+    const note = {
 
+        name: taskName,
 
+        completed: false,
 
+        date: taskDate
+    }
+
+    this.tasks.push(note)
     
+}
+
+TodoCreate.prototype.deleteTask =  function (name, apply) {
+
+    if (apply === true) {
+
+        this.tasks = this.tasks.filter(note => note.name !== name) 
+    }  
+}
+
+TodoCreate.prototype.editTask = function (taskName, newText, confirm) {
+
+    if (confirm === true) {
+        
+        this.tasks = this.tasks.map(function(task) {
+
+            if (task.name === taskName) {
+
+            task.text = newText
+        }
+            return task
+        })
+    }
     
+}
+
+TodoCreate.prototype.showInfoAboutTasks = function () {
+
+    let allTask = 0;
+
+    let activeTask = 0;
     
+    let completedTask = 0;
+
+    for (const [key,value] of Object.entries(this.tasks)) {
+
+         allTask++;
+
+         if (value.completed !== true) {
+
+             activeTask++;
+             
+         } else completedTask++;
+
+    }
+
+    console.log(`Всего задач - ${allTask}. Активных - ${activeTask}. Сделано - ${completedTask}`)
+}
+
+
+
+let todoList = new TodoCreate()
+
+todoList.createTask('task1')
+todoList.createTask('task2')
+todoList.showInfoAboutTasks()
+console.log(todoList)
+todoList.deleteTask('task2',true)
+todoList.editTask('task1','newtext,',true)
+
+
+
+console.log(todoList)
+
+
+
+let todoList2 = new TodoCreate()
+todoList2.createTask('task4')
+todoList2.createTask('task5')
+todoList2.showInfoAboutTasks()
+console.log(todoList2)
+todoList2.deleteTask('task5',true)
+todoList2.editTask('task4','newtext,',true)
+
+
+console.log(todoList2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
