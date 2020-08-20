9312d8f776b139ca0db8dd96cf6d488951b95907
ivanpavlenk <ivanpavlenko871@gmail.com>
2020-08-20 13:10:29 +0300

    homework15

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,14 +1,93 @@
-let heightTriangle = prompt('Введите число');
+class List {
 
-let triangle = ''
+    constructor (options) {
+        this.tasks = options.tasks
+    }
 
-for (let i = 1; i <= heightTriangle; i++) {
+    addTask(taskName) {
+        let note = {
+            name: taskName,
+            completed: false,
+            date: new Date().toLocaleString()
+        }
+        this.tasks.push(note)
+        this.addStorage()
 
-    triangle += "*".repeat(i) +'\n';
-    
+        
+    }
+
+    deleteTask(taskName) {
+        this.tasks = this.tasks.filter(note => note.name !== taskName)
+        localStorage.removeItem(taskName)
+    }
+
+    addStorage() {
+        let storage = this.tasks.map(function (note) {
+            localStorage.setItem(note.name, JSON.stringify(note))
+        })
+    }
+
 }
 
-alert(triangle);
+
+class TodoList extends List {
+
+    completed(taskName) {
+        this.tasks = this.tasks.map(function (note) {
+            localStorage.removeItem(taskName)
+            if (taskName === note.name) {
+                note.completed = true
+                localStorage.setItem(taskName,JSON.stringify(note))
+            }
+            return note
+        })
+    }
+
+    statystic() {
+
+        return this.tasks.reduce((accum,note) => {
+
+            if (note.completed) {
+                accum.completed++
+            }
+            return accum
+
+        },{allTask: this.tasks.length, completed: 0 })
+    }
+}
+
+
+class ContactList extends List {
+
+    search(val) {
+        this.tasks = this.tasks.filter(note => note.name === val || note.date === val)
+    }
+}
+
+
+let todoList = new TodoList({
+    tasks: []
+})
+
+let listSearch = new ContactList({
+    tasks: []
+})
+
+todoList.addTask('task');
+todoList.addTask('task2');
+todoList.completed('task2')
+
+console.log(todoList)
+
+
+
+
+
+
+
+
+
+
 
 
 
