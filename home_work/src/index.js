2bc12e8a59626a546bbcaa0b97b906f510907c15
ivan <ivanpavlenko871@gmail.com>
2020-09-02 22:53:20 +0300

    homework17

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,101 +1,66 @@
-class TodoList  {
 
-    constructor (baseUrl){
+let arr = [1,2,3,[4,5,6,7],8,9,]
 
-        this.baseUrl = baseUrl
-        this.notes = []
-        this.token = null
-    }
+let wrapper = document.querySelector('.wrapper')
 
-    auth(mail) {
-        if (mail.trim()) {
-            fetch(`${this.baseUrl}/auth/login`, {
 
-                method: 'POST',
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({
-                    value: mail
-                })
-               
-            })
-            .then(data => data.json())
-            .then(data => {
-                return localStorage.setItem('token',data.access_token)
-            })
-            .then(this.token = localStorage.getItem('token')) 
-        }  
-    }
+function generateList(array) {
 
-    initTodo() {
-        fetch(`${this.baseUrl}/todo`,{
-            method: 'GET',
-            headers:{ Authorization: `Bearer ${this.token}`}
-        })
-        .then(data => data.json())
-        .then(data => this.notes = data)
-    }
+    let list = document.createElement('ul')
 
-    addNote(value,priority) {
-        if (value.trim() && priority) {
-            fetch(`${this.baseUrl}/todo`,{
-                method: 'POST',
-                headers:{ 
-                    Authorization: `Bearer ${this.token}`,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    value,
-                    priority
-                })
-            })
-            .then(this.initTodo())
-        } 
-    }
+   for (let i = 0; i < array.length; i++) {
 
-    delete(id) {
-        fetch(`${this.baseUrl}/todo/${id}`,{
-            method: 'DELETE',
-            headers:{ Authorization: `Bearer ${this.token}`}
-        })
-        .then(this.initTodo())
-    }
+       if (!Array.isArray(array[i])) {
 
-    update(id,value,priority) {
-        if (id && value.trim && priority) {
-            fetch(`${this.baseUrl}/todo/${id}`,{
-                method: 'PUT',
-                headers:{ 
-                    Authorization: `Bearer ${this.token}`,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    value,
-                    priority
-                })
-            })
-            .then(this.initTodo())
-        } 
-    }
+            let listItem = document.createElement('li')
+            listItem.innerHTML = array[i]
+            list.append(listItem)
+        }
+
+        else  {
+
+            let newList = document.createElement('ul')
+            let innerLi = document.createElement('li')
+
+            for (let k = 0; k < array[i].length; k++) {
+                let newListItem = document.createElement('li')
+                newListItem.innerHTML = array[i][k]
+                newList.append(newListItem)
+                
+            }
+
+            innerLi.append(newList)
+            list.append(innerLi)
+             
+        }   
+   }
+
+    wrapper.append(list)
 }
 
-todo = new TodoList('https://todo.hillel.it')
-todo.auth('iserli35')
-todo.addNote('first2',1)
-todo.addNote('two3',2)
-setTimeout(() =>{
-    todo.update(2658,'test,', 1)
-},2000)
+generateList(arr)
 
 
 
 
-setTimeout(() => {
-    console.log(todo);
-    
-},7000)
+function generateTable() {
 
+    let table = document.createElement('table')
+    let numbers = 1
+    for (let i = 1; i <= 10; i++) {
+       
+        let tableRow = document.createElement('tr')
+       
+        for (let k = 1; k <= 10; k++) {
+          
+            let tableColumn = document.createElement('td')
+            tableColumn.innerHTML = numbers
+            tableRow.append(tableColumn)
+            numbers++
+        }  
+        table.append(tableRow)
+    }
+    wrapper.append(table)   
+}
 
-
-
-
-
+generateTable()
