d744bf48a4d8f858afe56bee0dcdb124989cc2cb
ivan <ivanpavlenko871@gmail.com>
2020-09-07 01:09:27 +0300

    homework18

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,66 +1,90 @@
+class TodoList {
 
-let arr = [1,2,3,[4,5,6,7],8,9,]
+    constructor() {
 
-let wrapper = document.querySelector('.wrapper')
-
-
-function generateList(array) {
-
-    let list = document.createElement('ul')
-
-   for (let i = 0; i < array.length; i++) {
-
-       if (!Array.isArray(array[i])) {
-
-            let listItem = document.createElement('li')
-            listItem.innerHTML = array[i]
-            list.append(listItem)
-        }
-
-        else  {
-
-            let newList = document.createElement('ul')
-            let innerLi = document.createElement('li')
-
-            for (let k = 0; k < array[i].length; k++) {
-                let newListItem = document.createElement('li')
-                newListItem.innerHTML = array[i][k]
-                newList.append(newListItem)
-                
-            }
-
-            innerLi.append(newList)
-            list.append(innerLi)
-             
-        }   
-   }
-
-    wrapper.append(list)
-}
-
-generateList(arr)
-
-
-
-
-function generateTable() {
-
-    let table = document.createElement('table')
-    let numbers = 1
-    for (let i = 1; i <= 10; i++) {
-       
-        let tableRow = document.createElement('tr')
-       
-        for (let k = 1; k <= 10; k++) {
-          
-            let tableColumn = document.createElement('td')
-            tableColumn.innerHTML = numbers
-            tableRow.append(tableColumn)
-            numbers++
-        }  
-        table.append(tableRow)
+      this.list = document.querySelector('.todo-list')
+      this.todoInput = document.querySelector('.todo-text')
+      this.btnAddNote = document.querySelector('.add')
+      this.toggleShowBtn = document.querySelector('.show-hide-complete')
     }
-    wrapper.append(table)   
-}
 
-generateTable()
+    todoInit() {
+
+        this.btnAddNote.addEventListener('click', () => {
+            let listItem = document.createElement('li')
+            listItem.classList.add('list-item')
+            listItem.setAttribute('data-complete','false')
+
+            let listText = document.createElement('p')
+            listText.innerText = this.todoInput.value
+    
+            let btnDone = document.createElement('button')
+            btnDone.classList.add('complete-btn')
+            btnDone.innerText = 'done'
+            this.completeNote(btnDone)
+           
+            let btnDelete = document.createElement('button')
+            btnDelete.classList.add('delete-btn')
+            btnDelete.innerText = 'delete'
+            this.deleteNote(btnDelete)
+            
+            let btnInner = document.createElement('div')
+            btnInner.append(btnDone)
+            btnInner.append(btnDelete)
+            listItem.append(listText)
+            listItem.append(btnInner)
+            this.list.append(listItem)
+            this.todoInput.value = ""
+        })
+    }
+
+    deleteNote(elem) {
+
+        elem.addEventListener('click',(event)=>{
+            let target = event.target
+            target.parentNode.parentNode.remove()
+        })
+    }
+
+    completeNote(elem) {
+
+        elem.addEventListener('click',function (event) {
+            let target = event.target
+            target.parentNode.parentNode.firstChild.classList.add('list-text')
+            this.disabled = true
+            target.parentNode.parentNode.setAttribute('data-complete','true')
+        })
+    }
+
+    toggleShowNote() {
+        this.toggleShowBtn.addEventListener('click',() => {
+            let listItems = document.querySelectorAll('.list-item')
+            listItems.forEach(elem => {
+                if (elem.getAttribute('data-complete') === "true") {
+                    elem.classList.toggle('list-item-hide')
+                }
+                
+            })
+            listItems.forEach(elem => {
+
+                if (elem.classList.contains('list-item-hide')) {
+                    this.toggleShowBtn.innerHTML = 'show complete'
+                }
+                else {
+                    this.toggleShowBtn.innerHTML = 'hide complete'
+                }
+            })
+        }) 
+
+    }
+  }
+    
+todo = new TodoList()
+todo.todoInit()
+todo.toggleShowNote()
+
+
+
+
+
+
