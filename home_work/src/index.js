5405fd4b5251eb00769c5be3ff093914745da7f6
ivan <ivanpavlenko871@gmail.com>
2020-09-18 15:25:14 +0300

    homework20repaire

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -97,25 +97,41 @@ function getRadians(degrees) {
 
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+var spotLight = new THREE.SpotLight(0xeeeece);
+spotLight.position.set(1000, 1000, 1000);
+
+scene.add(spotLight);
+var spotLight2 = new THREE.SpotLight(0xffffff);
+
+spotLight2.position.set( -200, -200, -200);
+scene.add(spotLight2);
+
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+camera.position.z = 50;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( {color: 0x776767} );
+var geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+var material = new THREE.MeshPhongMaterial( {
+    color: 0xdaa520,
+    specular: 0xbcbcbc,
+});
+var ring = new THREE.Mesh( geometry, material );
+scene.add( ring );
 
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+function animate () {
 
-var animate = function () {
     requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
+    ring.rotation.x += 0.01;
+    ring.rotation.y += 0.01;
     renderer.render( scene, camera );
 };
+
 animate();
 
 }   
+
+
